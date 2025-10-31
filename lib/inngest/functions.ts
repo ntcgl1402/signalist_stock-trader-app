43b8315ec9c6bca421e0b564734287313a38cc04
ntcgl1402 <ntcgl1402@gmail.com>
2025-10-31 15:58:01 +0700

    daily mail

--- lib/inngest/functions.ts
+++ lib/inngest/functions.ts
@@ -1,6 +1,7 @@
 import { inngest } from "@/lib/inngest/client";
 import { PERSONALIZED_WELCOME_EMAIL_PROMPT } from "./prompts";
 import { sendWelcomeEmail } from "../nodemailer";
+import { getAllUsersForNewsEmail } from "../action/user.action";
 
 export const sendSignUpEmail = inngest.createFunction(
     {id: "sign-up-email"},
@@ -40,6 +41,17 @@ export const sendSignUpEmail = inngest.createFunction(
         return {
             success: true,
             message: 'Welcome email sent successfully'
+        }
+    }
+)
+
+export const sendDailyNewsSummary = inngest.createFunction(
+    {id: "daily-news-summary"},
+    [{event: "app/send.daily.news"}, {cron: '0 12 * * *'} ],
+    async ({step}) => {
+        const users = await step.run('get-all-users', getAllUsersForNewsEmail)
+        if(!users || users.length === 0) {
+            return { success: false, message: 'No users to send news summary to' };
         }
     }
 )
